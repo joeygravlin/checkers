1b350e270a55b68b2ad701b1249d902e25664b58
Joseph Gravlin <joeygravlin@gmail.com>
2018-10-22 20:33:27 -0400

    Initial commit.

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'chinese-checkers'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
