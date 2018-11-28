c45690ff69c9adda8c690aae82bda608e6836a93
derrikf2 <fleminde@mail.gvsu.edu>
2018-11-28 13:47:15 -0500

    clean up & canAttack()

--- src/renderer/store/modules/Game.js
+++ src/renderer/store/modules/Game.js
@@ -2,44 +2,44 @@ class Game {
   constructor () {
     this.inProgress = false
     this.winner = null
-    this.currTurn = null
+    this.currentTurn = null
     this.squares = new Array(64).fill.map(square => new Square())
   }
 
   // TODO: Finish move
-  move(possIndex, currIndex) {
-    if (this.inProgress && isValidMove(possIndex, currIndex)) {
-      this.squares[possIndex].value = this.currentTurn
+  move(finalIndex, currIndex) {
+    if (this.inProgress && isValidMove(finalIndex, currIndex)) {
+      this.squares[finalIndex].value = this.currentTurn
       this.squares[currIndex].value = null
-
+      this.squares[currIndex].selected = false
 
       this.checkEndGame()
+      this.squares[finalIndex].canAttack = this.canAttack(finalIndex)
 
-      if (this.canAttack()) {
-        this.squares[possIndex].canAttack = true
+
+      else {
+        this.currentTurn = (this.currentTurn === Game.black) ? Game.white : Game.black
       }
-
-      this.currentTurn = (this.currentTurn === Game.red) ? Game.white : Game.red
     }
   }
 
   // TODO: Finish valid move check
-  isValidMove(possIndex, currIndex) {
+  isValidMove(finalIndex, currIndex) {
     valid = false
-    // if squares[possIndex] is empty AND
-    // if squares[possIndex] is valid AND
-    // if the diff between squares[possIndex] % 8 and squares[currIndex] is 1 (ie is it moving diagonally)
-    if (this.squares[possIndex].isValid === true &&
-      this.squares[possIndex].value === null &&
-      Math.abs((possIndex % 8) - (currIndex % 8)) === 1) {
+    // if squares[finalIndex] is empty AND
+    // if squares[finalIndex] is valid AND
+    // if the absolute diff between squares[finalIndex] % 8 and squares[currIndex] % 8 is 1 (ie is it moving diagonally)
+    if (this.squares[finalIndex].isValid === true &&
+      this.squares[finalIndex].value === null &&
+      Math.abs((finalIndex % 8) - (currIndex % 8)) === 1) {
       // if players piece is NOT a king
       if (this.squares[currIndex].isKing === false) {
         // if player is moving in the right direction (ie if it piece is white is it moving to the next row down (eg to an index in the next (printed) row) OR
-        // if it piece is red is moving to the previous row down (eg to an index in the previous (printed) row))
+        // if piece is white is moving to the previous row down (eg to an index in the previous (printed) row))
         if ((this.squares[currIndex].value === Game.white &&
-          Math.floor(possIndex / 8) - Math.floor(currIndex / 8) === 1) ||
+          Math.floor(finalIndex / 8) - Math.floor(currIndex / 8) === 1) ||
           (this.squares[currIndex].value === Game.black &&
-            Math.floor(possIndex / 8) - Math.floor(currIndex / 8) === -1)) {
+            Math.floor(finalIndex / 8) - Math.floor(currIndex / 8) === -1)) {
           valid = true
         }
       }
@@ -52,27 +52,50 @@ class Game {
   }
 
   canAttack(currIndex) {
-    if (this.squares[currIndex].isKing === false) {
+    canAttack = false
 
-    } else {
+    // direction for white
+    diagLeft = 7
+    diagRight = 9
 
+    // change direction if black
+    if (this.currentTurn === Game.black) {
+      diagLeft = -9
+      diagRight = -7
     }
+
+    // if either square in the "forward" direction does not equal the current players turn and is not null OR
+    // if either square in the "backwards" direction does not equal the current players turn and is not null AND players piece is a king:
+    // --> CAN ATTACK
+    if ((this.squares[currIndex + diagLeft].value != this.currentTurn
+      && this.squares[currIndex + diagLeft].value != null) ||
+      (this.squares[currIndex + diagRight].value != this.currentTurn
+        && this.squares[currIndex + diagRight].value != null) ||
+      (this.squares[currIndex].isKing === true &&
+        ((this.squares[currIndex - diagLeft].value != this.currentTurn &&
+          this.squares[currIndex - diagLeft].value != null) ||
+          (this.squares[currIndex - diagRight].value != this.currentTurn &&
+            this.squares[currIndex - diagRight].value != null)))) {
+      this.canAttack = true
+    }
+
+    return canAttack
   }
 
   checkEndGame() {
-    whitePieces = 0;
-    blackPieces = 0;
+    whitePieces = 0
+    blackPieces = 0
 
     squares.forEach(s => {
       if (s.value === Game.white)
-        redPieces++
-      if (s.value === Game.red)
+        whitePieces++
+      if (s.value === Game.black)
         blackPieces++
     })
 
-    if (redPieces === 0 || blackPieces === 0) {
+    if (whitePieces === 0 || blackPieces === 0) {
       this.inProgress = false
-      this.winner = (whitePieces < blackPieces) ? Game.white : Game.red
+      this.winner = (whitePieces < blackPieces) ? Game.white : Game.black
     }
   }
 
@@ -85,14 +108,14 @@ class Game {
         if (i <= 31)
           this.squares[i].value = Game.white
         if (i <= 48)
-          this.squares[i].value = Game.red
+          this.squares[i].value = Game.black
       }
     }
   }
 
   startGame() {
     this.inProgress = true
-    this.currTurn = Game.black
+    this.currentTurn = Game.black
     this.loadBoard()
     this.printBoard()
   }
