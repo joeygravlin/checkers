8d2fa1716f0573f852643fe15866b7281ee4f782
derrikf2 <fleminde@mail.gvsu.edu>
2018-11-28 23:56:44 -0500

    fix all eslint errors

--- src/renderer/components/Game/Game.js
+++ src/renderer/components/Game/Game.js
@@ -1,14 +1,16 @@
-class Game {
+import Square from './Square'
+
+export default class Game {
   constructor () {
     this.inProgress = true
     this.winner = null
     this.currentTurn = null
-    this.squares = new Array(64).fill.map(square => new Square())
+    this.squares = new Array(64).fill().map(s => new Square())
   }
 
   // TODO: Finish move
-  move(finalIndex, currIndex) {
-    if (this.inProgress && isValidMove(finalIndex, currIndex)) {
+  move (finalIndex, currIndex) {
+    if (this.inProgress && this.isValidMove(finalIndex, currIndex)) {
       this.squares[finalIndex].value = this.currentTurn
       this.squares[currIndex].value = null
       this.squares[currIndex].selected = false
@@ -19,17 +21,15 @@ class Game {
       if (this.canAttack(finalIndex)) {
         this.squares[finalIndex].canAttack = true
         this.squares[finalIndex].selected = true
-
-      }
-      else {
+      } else {
         this.currentTurn = (this.currentTurn === Game.black) ? Game.white : Game.black
       }
     }
   }
 
   // TODO: Finish valid move check
-  isValidMove(finalIndex, currIndex) {
-    valid = false
+  isValidMove (finalIndex, currIndex) {
+    var valid = false
 
     // if squares[finalIndex] is empty
     // AND
@@ -39,10 +39,8 @@ class Game {
     if (this.squares[finalIndex].isValid === true &&
       this.squares[finalIndex].value === null &&
       Math.abs((finalIndex % 8) - (currIndex % 8)) === 1) {
-
       // if players piece is NOT a king
       if (this.squares[currIndex].isKing === false) {
-
         // if player is moving in the right direction (ie if it piece is white is it moving to the next row down (eg to an index in the next (printed) row)
         // OR
         // if piece is white is moving to the previous row down (eg to an index in the previous (printed) row))
@@ -52,23 +50,21 @@ class Game {
             Math.floor(finalIndex / 8) - Math.floor(currIndex / 8) === -1)) {
           valid = true
         }
-      }
-      else {
+      } else {
         valid = true
       }
     }
-
     return valid
   }
 
-  canAttack(currIndex) {
-    canAttack = false
+  canAttack (currIndex) {
+    var canAttack = false
 
-    //diag direction Game.white
-    diagLeft = 7
-    diagRight = 9
+    // diag direction Game.white
+    var diagLeft = 7
+    var diagRight = 9
 
-    //diag direction Game.black
+    // diag direction Game.black
     if (this.currentTurn === Game.black) {
       diagLeft = -9
       diagRight = -7
@@ -78,30 +74,30 @@ class Game {
     // OR
     // if either square in the "backwards" direction does not equal the current players turn and is not null AND players piece is a king:
     // --> CAN ATTACK
-    if ((this.squares[currIndex + diagLeft].value != this.currentTurn
-      && this.squares[currIndex + diagLeft].value != null) ||
-      (this.squares[currIndex + diagRight].value != this.currentTurn
-        && this.squares[currIndex + diagRight].value != null) ||
+    if ((this.squares[currIndex + diagLeft].value !== this.currentTurn && this.squares[currIndex + diagLeft].value !== null) ||
+      (this.squares[currIndex + diagRight].value !== this.currentTurn &&
+      this.squares[currIndex + diagRight].value !== null) ||
       (this.squares[currIndex].isKing === true &&
-        ((this.squares[currIndex - diagLeft].value != this.currentTurn &&
-          this.squares[currIndex - diagLeft].value != null) ||
-          (this.squares[currIndex - diagRight].value != this.currentTurn &&
-            this.squares[currIndex - diagRight].value != null)))) {
+        ((this.squares[currIndex - diagLeft].value !== this.currentTurn &&
+          this.squares[currIndex - diagLeft].value !== null) ||
+          (this.squares[currIndex - diagRight].value !== this.currentTurn &&
+            this.squares[currIndex - diagRight].value !== null)))) {
       this.canAttack = true
     }
 
     return canAttack
   }
+  checkEndGame () {
+    var whitePieces = 0
+    var blackPieces = 0
 
-  checkEndGame() {
-    whitePieces = 0
-    blackPieces = 0
-
-    squares.forEach(s => {
-      if (s.value === Game.white)
+    this.squares.forEach(s => {
+      if (s.value === Game.white) {
         whitePieces++
-      if (s.value === Game.black)
+      }
+      if (s.value === Game.black) {
         blackPieces++
+      }
     })
 
     if (whitePieces === 0 || blackPieces === 0) {
@@ -110,33 +106,38 @@ class Game {
     }
   }
 
-  loadBoard() {
+  loadBoard () {
+    var i
     for (i = 0; i < 64; i++) {
-      //setting squares as valid, putting pieces "on" board
+      // setting squares as valid, putting pieces "on" board
       if ((Math.floor(i / 8) % 2 === 0 && (i % 2) === 0) ||
         (Math.floor(i / 8) % 2 === 1 && (i % 2) === 1)) {
         this.squares[i].isValid = true
-        if (i <= 31)
+        if (i <= 31) {
           this.squares[i].value = Game.white
-        if (i <= 48)
+        }
+        if (i <= 48) {
           this.squares[i].value = Game.black
+        }
       }
     }
   }
 
-  startGame() {
+  startGame () {
     this.inProgress = true
     this.currentTurn = Game.black
     this.loadBoard()
     this.printBoard()
   }
 
-  //testing
-  printBoard() {
+  // testing
+  printBoard () {
+    var i
     for (i = 0; i < 64; i++) {
-      console.log("| " + this.squares[i].value + " | ")
-      if (i % 8 === 7)
-        console.log("\n")
+      console.log('| ' + this.squares[i].value + ' | ')
+      if (i % 8 === 7) {
+        console.log('\n')
+      }
     }
   }
 }
--- src/renderer/components/Game/Game.vue
+++ src/renderer/components/Game/Game.vue
@@ -1,32 +1,32 @@
 <template>
   <div>
     <!-- TODO: Build build board here (idea: for(each) square in this.game.squares make a div to represent the square) -->
+    here's the app
   </div>
 </template>
 
 <script>
-  import Game from "./Game.js";
-  import Square from "./Square.js";
+  import Game from './Game.js'
 
   export default {
-    name: "game",
+    name: 'game',
 
-    data() {
+    data () {
       return {
         // TODO: Should discuss
-      };
+      }
     },
 
     methods: {
-      start() {
-        this.clearBoard();
-        this.game = new Game();
-        //TODO: moar?
+      start () {
+        this.clearBoard()
+        this.game = new Game()
+        // TODO: moar?
       },
 
-      clearBoard() {
-        //TODO: Necessary?
+      clearBoard () {
+        // TODO: Necessary?
       }
     }
-  };
+  }
 </script>
