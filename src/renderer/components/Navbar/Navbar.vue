2bf47d487a182b352907989c19d0a28486e90df9
Al Duncanson <alexjduncanson@gmail.com>
2018-11-30 20:45:43 -0500

    got rid of bootstrap

--- src/renderer/components/Navbar/Navbar.vue
+++ src/renderer/components/Navbar/Navbar.vue
@@ -1,17 +1,9 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link to="/" class="navbar-brand">Checkers</router-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <router-link to="/game" activeClass="active" tag="li"><a class="nav-link">Game</a></router-link>
+    <ul id="nav">
+        <router-link to="/game" activeClass="active" tag="li"><a class="nav-link">Play</a></router-link>
         <router-link to="/connect" activeClass="active" tag="li"><a class="nav-link">Connect</a></router-link>
-      </ul>
-    </div>
-  </nav>
+        <li>Dark Mode</li>
+    </ul>
 </template>
 
 <script>
@@ -19,4 +11,12 @@ export default {
   name: 'navbar'
 }
 </script>
+
+<style lang="sass" scoped>
+
+    #nav
+        display: flex
+
+</style>
+
 
