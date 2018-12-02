806be5e7c063544b6655ad97676147406c2b80ba
derrikf2 <fleminde@mail.gvsu.edu>
2018-12-02 17:29:51 -0500

    square color

--- src/renderer/components/Game/Square.js
+++ src/renderer/components/Game/Square.js
@@ -1,6 +1,9 @@
+import { runInThisContext } from "vm";
+
 export default class Square {
   constructor () {
     this.value = null
+    this.color = null
     this.isSelected = false
     this.canAttack = false
     this.isKing = false
