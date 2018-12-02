698b8e920bd59679c187ad2396102f5f4e357dc7
Joseph Gravlin <joeygravlin@gmail.com>
2018-12-02 17:40:34 -0500

    Move square styles to Square component.

--- src/renderer/components/Game/Game.vue
+++ src/renderer/components/Game/Game.vue
@@ -89,42 +89,4 @@
                 display: flex
                 flex-direction: row
 
-                .square
-                    height: 60px
-                    width: 60px
-                    position: relative
-
-                    &.white
-                        background: #ffffff
-
-                        &.hasWhitePiece
-                            &:before
-                                content: ''
-                                border: 1px solid #000000
-                                width: 40px
-                                height: 40px
-                                position: absolute
-                                border-radius: 21px
-                                top: 9px
-                                left: 9px
-                                background-color: #ffffff
-                                box-shadow: 2px 2px 2px #000000
-                                cursor: pointer
-
-                        &.hasBlackPiece
-                            &:before
-                                content: ''
-                                border: 1px solid #000000
-                                width: 40px
-                                height: 40px
-                                position: absolute
-                                border-radius: 21px
-                                top: 9px
-                                left: 9px
-                                background-color: #000000
-                                box-shadow: 2px 2px 2px #000000
-                                cursor: pointer
-
-                    &.black
-                        background: #000000
 </style>
--- src/renderer/components/Game/Square.vue
+++ src/renderer/components/Game/Square.vue
@@ -30,14 +30,43 @@
 
 <style lang="sass" scoped>
 
-  square
-    height: 60px
-    width: 60px
+  .square
+      height: 60px
+      width: 60px
+      position: relative
 
-    &.white
-        background: #ffffff
+      &.white
+          background: #ffffff
 
-    &.black
-        background: #000000
+          &.hasWhitePiece
+              &:before
+                  content: ''
+                  border: 1px solid #000000
+                  width: 40px
+                  height: 40px
+                  position: absolute
+                  border-radius: 21px
+                  top: 9px
+                  left: 9px
+                  background-color: #ffffff
+                  box-shadow: 2px 2px 2px #000000
+                  cursor: pointer
+
+          &.hasBlackPiece
+              &:before
+                  content: ''
+                  border: 1px solid #000000
+                  width: 40px
+                  height: 40px
+                  position: absolute
+                  border-radius: 21px
+                  top: 9px
+                  left: 9px
+                  background-color: #000000
+                  box-shadow: 2px 2px 2px #000000
+                  cursor: pointer
+
+      &.black
+          background: #000000
 
 </style>
