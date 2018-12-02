c15f785c66e0a0f31ebf557e592b247345996691
Al Duncanson <alexjduncanson@gmail.com>
2018-12-02 14:47:21 -0500

    styled new menu

--- src/renderer/components/Navbar/Navbar.vue
+++ src/renderer/components/Navbar/Navbar.vue
@@ -1,9 +1,11 @@
 <template>
-    <ul id="nav">
-        <router-link to="/game" activeClass="active" tag="li"><a class="nav-link">Play</a></router-link>
-        <router-link to="/connect" activeClass="active" tag="li"><a class="nav-link">Connect</a></router-link>
-        <li>Dark Mode</li>
-    </ul>
+    <div id="menu">
+        <ul>
+            <router-link to="/game" activeClass="active" tag="li"><a class="nav-link">Play</a></router-link>
+            <router-link to="/connect" activeClass="active" tag="li"><a class="nav-link">Connect</a></router-link>
+            <li>Dark Mode</li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -14,8 +16,33 @@ export default {
 
 <style lang="sass" scoped>
 
-    #nav
-        display: flex
+    #menu
+        background-color: #ffffff
+
+        ul
+            list-style-type: none
+            margin: 0
+            padding: 0
+            display: flex
+
+            li
+                padding: 5px
+                margin: 5px
+                box-shadow: 3px 2px 6px #000000
+                transition: hover 1s
+                user-select: none
+                text-align: center
+
+                &:hover
+                    box-shadow: 5px 4px 8px #000000
+                    cursor: pointer
+
+                &:active
+                    box-shadow: 1px 0px 4px #000000
+
+                a
+                    color: #000000
+                    text-decoration: none
 
 </style>
 
