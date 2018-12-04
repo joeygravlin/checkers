c7b40e80ddb59cc547272c66c52d2078e8dc6004
Joseph Gravlin <joeygravlin@gmail.com>
2018-12-04 10:35:40 -0500

    Add input form to Connect.vue

--- src/renderer/components/Connect/Connect.vue
+++ src/renderer/components/Connect/Connect.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1 @click="connect">Connect to an opponent Player!</h1>
+  <div>
+    <h1>Connect to an opponent Player!</h1>
+    <label for="host">Host Address: </label>
+    <input v-model="host" id="host" placeholder="enter opponent's host address">
+    <label for="port">Port: </label>
+    <input v-model="port" id="port" placeholder="enter opponent's listening port">
+    <button @click="connect">Connect to an opponent Player!</button>
+  </div>
 </template>
 
 <script>
@@ -7,6 +14,10 @@ import Game from '../Game/Game'
 
 export default {
   name: 'connect',
+  // props: {
+  //   host: String,
+  //   port: Number
+  // },
   data () {
     return {
       host: '127.0.0.1',
