c82d47d02bbafb36d05362330a20d356145f95f3
Joseph Gravlin <joeygravlin@gmail.com>
2018-12-05 16:20:56 -0500

    Remove dark-mode option from Navbar.vue

--- src/renderer/components/Navbar/Navbar.vue
+++ src/renderer/components/Navbar/Navbar.vue
@@ -3,17 +3,13 @@
         <ul>
             <router-link to="/game" activeClass="active" tag="li"><a class="nav-link">Play</a></router-link>
             <router-link to="/connect" activeClass="active" tag="li"><a class="nav-link">Connect</a></router-link>
-            <li @click="enableDarkMode()">Dark Mode</li>
         </ul>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'navbar',
-  methods: {
-    enableDarkMode () {}
-  }
+  name: 'navbar'
 }
 </script>
 
